5025a663450280b0d70902d9026ca02bc466af67
吴芷文 <596938841@qq.com>
2021-08-09 15:32:28 +0800

    first commit

---
+++ src/main/java/com/example/mypro/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.example.mypro.dao;
+
+public interface UserDao {
+    // 增加用户
+    public int addUser(int id, String name, String password);
+    // 根据ID删除用户
+    public int deleteUserById(int id);
+    // 根据ID查询用户
+    public int queryUserById(int id);
+    // 修改用户
+
+}
---
+++ src/test/java/com/example/mypro/MyProApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.mypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+class MyProApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
